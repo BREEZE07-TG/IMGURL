0a5aefe9d62854ef8c75bea8cf352af67d063f3e
Akshadgod <157225495+Akshadgod@users.noreply.github.com>
2024-10-12 10:23:03 +0530

    Update __init__.py

--- IMGURL/__init__.py
+++ IMGURL/__init__.py
@@ -1,1 +1,17 @@
+from pyrogram import Client , filters
+import logging
+import config as c
 
+FORMAT = "[IMGURL]: %(message)s"
+
+logging.basicConfig(level=logging.INFO, handlers=[logging.FileHandler('logs.txt'),
+                                                   logging.StreamHandler()], format=FORMAT)
+plugins = {"root":"module"}
+
+app = Client(
+    "IMGURL", 
+    api_id=c.api_id, 
+    api_hash=c.api_hash,
+    bot_token=c.bot_token,
+    plugins=plugins
+)
