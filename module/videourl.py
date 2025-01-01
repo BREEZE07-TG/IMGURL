bd91c47fe59f491920258a3a77ef5ff226479c93
Akshadgod <157225495+Akshadgod@users.noreply.github.com>
2025-01-01 13:32:43 +0530

    Update videourl.py

--- module/videourl.py
+++ module/videourl.py
@@ -22,7 +22,7 @@ from io import BytesIO
 #app = Client("my_account")
 
 @app.on_message(filters.command("licence"))
-def licence(_, message):
+async def licence(_, message):
     img_url = "https://i.ibb.co/PDTjQWG/photo-2025-01-01-03-45-46-7454788247634313240.jpg"
     response = requests.get(img_url)
     imag = Image.open(BytesIO(response.content))
@@ -33,12 +33,12 @@ def licence(_, message):
     draw.text(points, string, "white", font=font1)
     imag.save("someone.jpg")
 
-    message.reply_photo("someone.jpg")
+    await message.reply_photo("someone.jpg")
 
     # Remove the temporary image file
     os.remove("someone.jpg")
 
-app.run()
+#app.run()
 
 
 #This code uses the `os.remove()` function to delete the "someone.jpg" file after it's been sent as a photo. This helps keep your temporary files organized and avoids cluttering your file system.
