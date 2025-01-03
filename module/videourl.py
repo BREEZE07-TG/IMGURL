bf9cb5b90d16a6cc047891600a7ac9681990b224
Akshadgod <157225495+Akshadgod@users.noreply.github.com>
2025-01-03 08:27:15 +0530

    Update videourl.py

--- module/videourl.py
+++ module/videourl.py
@@ -1,25 +1,8 @@
-import os
-import time
 import requests
 from pyrogram import Client, filters
 from IMGURL import app
-
-#Here's an example of how you can create a `/licence` command using Pyrogram that generates an image with the specified text:
-#from pyrogram import Client, filters
 from PIL import Image, ImageDraw, ImageFont
-#import requests
 from io import BytesIO
-
-#Here's the modified code that uses the `os` module to remove the temporary image file after sending it:
-
-
-#from pyrogram import Client, filters
-#from PIL import Image, ImageDraw, ImageFont
-#import requests
-#from io import BytesIO
-#import os
-
-#app = Client("my_account")
 
 @app.on_message(filters.command("licence"))
 async def licence(_, message):
@@ -35,10 +18,5 @@ async def licence(_, message):
 
     await message.reply_photo("someone.jpg")
 
-    # Remove the temporary image file
+    
     os.remove("someone.jpg")
-
-#app.run()
-
-
-#This code uses the `os.remove()` function to delete the "someone.jpg" file after it's been sent as a photo. This helps keep your temporary files organized and avoids cluttering your file system.
