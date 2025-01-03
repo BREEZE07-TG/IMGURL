5396406632ed14ae000de7a612eac306b4a08ec1
Akshadgod <157225495+Akshadgod@users.noreply.github.com>
2025-01-03 08:34:23 +0530

    Update __main__.py

--- IMGURL/__main__.py
+++ IMGURL/__main__.py
@@ -1,7 +1,10 @@
-
+import time
 from IMGURL import app
 
 print("STARTED💫")
 
 if __name__=="__main__":
   app.run()
+  with app:
+    time.sleep(15)
+    app.send_message(-1002011444793,"Bot has been started!")
