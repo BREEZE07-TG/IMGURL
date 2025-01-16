f706e20b8f2a23b458eee0b014546265c8ed24ca
BREEZE <157225495+BREEZE07-TG@users.noreply.github.com>
2025-01-16 07:35:00 +0530

    Update __init__.py

--- IMGURL/__init__.py
+++ IMGURL/__init__.py
@@ -7,12 +7,11 @@ FORMAT = "[IMGURL]: %(message)s"
 
 logging.basicConfig(level=logging.INFO, handlers=[logging.FileHandler('logs.txt'),
                                                    logging.StreamHandler()], format=FORMAT)
-plugins = {"root":"module"}
 
 app = Client(
     "IMGURL", 
     api_id=c.api_id, 
     api_hash=c.api_hash,
     bot_token=c.bot_token,
-    plugins=plugins
+    plugins=dict(root='module')
 )
