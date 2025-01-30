ad6fa893525a7c54fb8fc6c7d20fadf708f0e591
BREEZE <157225495+BREEZE07-TG@users.noreply.github.com>
2025-01-30 22:56:10 +0530

    Update start.py

--- module/start.py
+++ module/start.py
@@ -3,8 +3,10 @@ from pyrogram import Client, filters
 
 @app.on_message(filters.command('start'))        
 async def start_handler(client,message):
-               await client.send_photo(message.chat.id,"https://i.ibb.co/nMSHkfJ/photo-2024-10-03-17-58-50-7421610415546695696.jpg",
-               "**--This is an advanced image to url genrater bot--**\n\n"
-               ">What can this bot do:\n"
-               "<pre>Currently this bot can provide any photo url under 50mb\nI hope many features would be added soon! \nplease send me any photo to continue</pre>")       
+               await client.send_photo(chat_id = message.chat.id,
+                                       photo = "https://i.ibb.co/nMSHkfJ/photo-2024-10-03-17-58-50-7421610415546695696.jpg",
+                                       caption = "**--This is an advanced image to url genrater bot--**\n\n"
+                                       ">What can this bot do:\n"
+                                       "<pre>Currently this bot can provide any photo url under 50mb\nI hope many features would be added soon! \nplease send me any photo to continue</pre>",
+                                       reply_to_message_id= message.chat.id)       
 
