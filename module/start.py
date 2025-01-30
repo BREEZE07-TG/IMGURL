bcc25c121e0d0c0fe0c5c01f27c478c8d7b116d1
BREEZE <157225495+BREEZE07-TG@users.noreply.github.com>
2025-01-30 23:02:28 +0530

    Update start.py

--- module/start.py
+++ module/start.py
@@ -1,12 +1,16 @@
 from IMGURL import app
-from pyrogram import Client, filters 
+from pyrogram import Client, filters  
 
 @app.on_message(filters.command('start'))        
-async def start_handler(client,message):
-               await client.send_photo(chat_id = message.chat.id,
-                                       photo = "https://i.ibb.co/nMSHkfJ/photo-2024-10-03-17-58-50-7421610415546695696.jpg",
-                                       caption = "**--This is an advanced image to url genrater bot--**\n\n"
-                                       ">What can this bot do:\n"
-                                       "<pre>Currently this bot can provide any photo url under 50mb\nI hope many features would be added soon! \nplease send me any photo to continue</pre>",
-                                       reply_to_message_id= message.message.id)       
-
+async def start_handler(client, message):
+    await client.send_photo(
+        chat_id=message.chat.id,
+        photo="https://i.ibb.co/nMSHkfJ/photo-2024-10-03-17-58-50.jpg",
+        caption="**--This is an advanced image to URL generator bot--**\n\n"
+                "> What can this bot do:\n"
+                "<pre>Currently, this bot can provide any photo URL under 50MB.\n"
+                "I hope many features will be added soon!\n"
+                "Please send me any photo to continue.</pre>",
+        reply_to_message_id=message.message_id
+    )
+  
