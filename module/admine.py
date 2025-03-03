1b844ac01aadce6858b144f63c24f1f8c024022a
BREEZE <157225495+BREEZE07-TG@users.noreply.github.com>
2025-03-03 15:50:20 +0530

    Update admine.py

--- module/admine.py
+++ module/admine.py
@@ -4,14 +4,21 @@ import subprocess
 import os
 import asyncio
 import sys
+from module import exec as e
 
 @app.on_message(filters.command('logs'))
 async def logs(client,message):
+  if message.from_user.id != e.owner:
+    await message.reply("You are not authorised to use this command")
+    return 
   L = subprocess.getoutput("tail -n 20 logs.txt")
   await message.reply(f"<pre>Logs\n{L}</pre>")
 
 @app.on_message(filters.command('restart'))
 async def restart(client,message):
+  if message.from_user.id != e.owner:
+    await message.reply("You are not authorised to use this command")
+    return 
   rst_msg= await message.reply("restarting system...")
   await asyncio.sleep(2)
   subprocess.run(["python", "-m", "restart"])
