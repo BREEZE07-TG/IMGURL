6c235ce70a2685483ff55799270fa74af280b8b6
BREEZE <157225495+BREEZE07-TG@users.noreply.github.com>
2025-04-03 13:01:13 +0530

    Update start.py

--- module/start.py
+++ module/start.py
@@ -1,23 +1,25 @@
 from IMGURL import app
 from pyrogram import Client, filters  
-from pyrogram.types import InlineKeyboardButton , InlineKeyboardMarkup
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 @app.on_message(filters.command('start'))        
 async def start_handler(client, message):
     await client.send_photo(
         chat_id=message.chat.id,
         photo="https://i.ibb.co/nMSHkfJ/photo-2024-10-03-17-58-50.jpg",
-        caption="**--This is an advanced image to URL generator bot--**\n\n"
-                "> What can this bot do:\n"
-                "<pre>Supports multi media Url\n"
-                "Usage: \n"
-                "In group use /url & in pvt directly send that media\n</pre>"\n\n
-                f"{"➖"*15}\n"
-                "Click on \"manual\" for more details"\n
-                "➖"*15,
-                reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("manual",url="https://test-oirz.onrender.com")]])
-                
+        caption=(
+            "**--This is an advanced image to URL generator bot--**\n\n"
+            "> **What can this bot do:**\n"
+            "<pre>Supports multi-media URLs\n"
+            "Usage: \n"
+            "In group: use /url\n"
+            "In private: send media directly</pre>\n\n"
+            "➖" * 15 + "\n"
+            "Click on \"Manual\" for more details\n"
+            "➖" * 15
+        ),
+        reply_markup=InlineKeyboardMarkup([
+            [InlineKeyboardButton("Manual", url="https://test-oirz.onrender.com")]
+        ]),
         reply_to_message_id=message.id
     )
-    
-  
